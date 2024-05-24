bfd93c2f8f36268fda84bdc9d039b51a83775efc
auddnjssk <nxnx23@naver.com>
2024-05-24 11:14:45 +0900

    first

---
+++ src/main/java/PratiqueGW/PratiqueGW/PratiqueGwApplication.java
@@ -0,0 +1,13 @@
+package PratiqueGW.PratiqueGW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PratiqueGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PratiqueGwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PratiqueGW/PratiqueGW/PratiqueGwApplicationTests.java
@@ -0,0 +1,13 @@
+package PratiqueGW.PratiqueGW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratiqueGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
